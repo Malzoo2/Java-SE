3e6baaf6238f141f3e7ac567b55892b83b0db4b5
mabdelaal <Malzoo2@hotmail.com>
2022-03-31 11:29:54 +0300

    rtyuty

--- Java-SE/src/com/abdelaal/types.java
+++ Java-SE/src/com/abdelaal/types.java
@@ -4,7 +4,7 @@ public class types {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("sdfdaddfffffgdfgdfgsdff");
+		System.out.println("sdfdaddfgdfgdfgsdff");
 
 	}
 
